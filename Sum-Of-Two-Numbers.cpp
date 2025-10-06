5e7180f1e3ef4d0f026bd8569cbd98b12fa0f306
unknown <alelybitun.school@gmail.com>
2025-10-06 11:19:12 +0800

    feat: Add Sum of Two Numbers

---
+++ Sum-Of-Two-Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+using namespace std;
+
+int main (){
+
+    cout << "Sun of the Two Number" << endl;
+    int x, y;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+
+    cout << "The sum of of the two numbers: " << x+y << endl;
+
+
+    return 0;
+}
