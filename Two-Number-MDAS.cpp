065535654bd202ecfdc95862dee5f39a54414885
unknown <alelybitun.school@gmail.com>
2025-10-06 11:35:34 +0800

    feat: Add MDAS of the Two Numbers

---
+++ Two-Number-MDAS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    cout << "Sun of the Two Number" << endl;
+    int x, y;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+
+    cout << "The mutiplication of the two numbers: " << x*y << endl;
+    cout << "The division of the two numbers: " << x/y << endl;
+    cout << "The addition of the two numbers: " << x+y << endl;
+    cout << "The subtraction of the two numbers: " << x-y << endl;
+    
+    return 0;
+}
