f0f3ac78796caec6c8778bb0ab329887066b4ee1
unknown <alelybitun.school@gmail.com>
2025-10-07 10:36:54 +0800

    feat: SinglyLinkedList: Delete First Node

---
+++ 1LinkedList-DeleteFirst.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+
+};
+
+struct Node *head = NULL;
+
+void insertNode (int n){
+    struct Node *new_node = new Node();
+    new_node->data = n;
+    new_node->next = head;
+    head=new_node;
+
+}
+
+void deleteFirstNode(){
+    if (head != nullptr){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+    
+}
+
+
+
+int main(){
+
+    int choice, num;
+    char exit;
+    exit = 'N';
+    while (exit == 'N'|| exit == 'n'){
+
+        cout << "Singly Linked List" << endl;
+        cout << "1. Add a number" << endl;
+        cout << "2. Display number" << endl;
+        cout << "3. Exit" << endl;
+        cin >> choice;
+
+        switch(choice){
+            case 1:{
+                cout << "What number do you want to added from the list? ";
+                cin >> num;
+                insertNode(num);
+            break;}
+            case 2:
+                deleteFirstNode();
+            break;
+            case 3:
+                cout << "Do you want to exit the Program? [Y/N]: ";
+                cin >> exit;
+            break;
+            default:
+            break;
+        }
+    }
+
+
+
+    return 0;
+}
